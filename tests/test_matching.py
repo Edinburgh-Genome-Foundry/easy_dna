335bff54d48cc83b24aa53634da503405242d21a
Peter Vegh <pvegh@posteo.uk>
2020-03-25 14:26:57 +0000

    Added test for find_occurence()

--- tests/test_matching.py
+++ tests/test_matching.py
@@ -1,5 +1,5 @@
 from easy_dna import dna_pattern_to_regexpr, all_iupac_variants
-from easy_dna.matching import find_index
+from easy_dna.matching import find_index, find_occurence
 
 
 def test_dna_pattern_to_regexpr():
@@ -12,3 +12,9 @@ def test_all_iupac_variants():
 
 def test_find_index():
     assert find_index("ATCGATTCGATC", "CGA") == 2
+
+
+def test_find_occurence():
+    assert find_occurence('ATCGATTCGATC', 'CGA', strand="both") == (2, 5, 1)
+    assert find_occurence('ATCGATTCGATC', 'AAAAA', strand="both") == None
+    assert find_occurence('ATCGATTCGATC', 'AATC', strand="both") == (3, 7, -1)
