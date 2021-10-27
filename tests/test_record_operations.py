2e4985f6efb92aa734ad1b58e1048fe5c0fd9314
Peter Vegh <pvegh@posteo.uk>
2021-10-27 13:43:55 +0100

    Test censor_record()

--- tests/test_record_operations.py
+++ tests/test_record_operations.py
@@ -3,6 +3,7 @@ from easy_dna import (
     sequence_to_biopython_record,
     record_with_different_sequence,
     anonymized_record,
+    censor_record,
 )
 
 
@@ -17,3 +18,14 @@ def test_anonymized_record():
     assert record.name == "anonymized"
     assert record.features[0].qualifiers["label"] == "feature_0"
 
+
+def test_censor_record():
+    record = sequence_to_biopython_record(
+        "ACGTGCGATGGGATTATTTCCAAC", id="test id", name="test name"
+    )
+    annotate_record(record, location="full", feature_type="test_feature")
+    record = censor_record(record)
+    assert record.seq != "ACGTGCGATGGGATTATTTCCAAC"
+    assert record.id == "censored"
+    assert record.name == "censored"
+    assert record.features[0].qualifiers["label"] == "feature_0"
